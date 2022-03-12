4ea1d4bf306101b44b67f68b640b8167086b2aa3
rsc2143 <rohitschauhanitbhu@gmail.com>
2022-03-12 23:46:10 +0530

    Update settings.py

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == "1"
+# DEBUG = str(os.environ.get('DEBUG')) == "1"
+DEBUG = True
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
 ALLOWED_HOSTS = []
 if ENV_ALLOWED_HOST:
